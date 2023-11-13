750f73a89330518db67aed27fa6a5b4d078f92b6
nic00la1 <nic00la.kaleta@gmail.com>
2023-11-13 19:12:16 +0100

    adding logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   standalone: true,
   selector: 'app-root',
-  template: `<h1>Hello Universe!</h1>`,
+  template: `
+    <main>
+      <header class="brand-name">
+        <img
+          class="brand-logo"
+          src="/assets/logo.svg"
+          alt="logo"
+          aria-hidden="true"
+        />
+      </header>
+    </main>
+  `,
+
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
